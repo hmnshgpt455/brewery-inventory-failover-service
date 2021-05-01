f73273061910ec21026df64ef1342c5029b5968f
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-05-01 19:45:38 +0530

    Correcting BeerInventoryDto. Closes #3.

--- src/main/java/io/github/hmnshgpt455/brewery/model/BeerInventoryDto.java
+++ src/main/java/io/github/hmnshgpt455/brewery/model/BeerInventoryDto.java
@@ -1,6 +1,5 @@
 package io.github.hmnshgpt455.brewery.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,11 +21,7 @@ public class BeerInventoryDto implements Serializable {
     static final long serialVersionUID = 9031510786210771298L;
 
     private UUID id;
-
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
-
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     private UUID beerId;
     private Integer quantityOnHand;
